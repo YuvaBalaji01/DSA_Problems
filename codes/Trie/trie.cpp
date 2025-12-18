ae2e0f89e87fb79daf820564f35f130f3aabb1c8
YuvaBalaji01 <yuvabalajikumar@gmail.com>
2025-12-18 16:41:10 +0530

    updateted code

--- codes/Trie/trie.cpp
+++ codes/Trie/trie.cpp
@@ -12,30 +12,83 @@ void insertion(string word,Node* root){
         if(root->child[index] == NULL) {
 
             Node* newindex = new Node(word[i]);
-            root->child[i] = newindex;
-            root = root->child[i];
-            delete newindex;
+            root->child[index] = newindex;
+            root = root->child[index];
+            
         }
 
         else {
 
-            root = root->child[i];
+            root = root->child[index];
         }
-
-        root->isterminal = true;
+      
     }
+
+      root->cnt++; 
 
 
 }
 
+void remove(string word , Node* head){
+
+    for(int i= 0;i<word.length();i++){
+
+        int index = word[i] - 'a';
+
+        if(head->child[index] != NULL) head = head->child[index];
+        else return;
+    }
+
+    head->cnt--;
+}
+
+void print(Node* root, string &temp) {
+
+    if (root->cnt > 0) {
+        cout << temp << endl;
+    }
+
+    for (int i = 0; i < 26; i++) {
+
+        if (root->child[i] != NULL) {
+            temp.push_back(root->child[i]->data);
+            print(root->child[i], temp);
+            temp.pop_back();  
+        }
+    }
+}
+
+
+bool find(string word,Node* root){
+
+    for(int i=0;i<word.length();i++){
+
+        int index = word[i] -'a';
+
+        if(root->child[index] != NULL) root = root->child[index];
+        else return false;
+    }
+
+    if(root->cnt > 0) return true ;
+    return false;
+}
+
 int main(){
 
-    string word;
-    cin >> word;
-
     Node * root = new Node('$');
+     
+    int n;
+    cin >> n;
+    while(n--){
 
-    insertion(word,root);
+       string word;
+       cin >> word;
+       insertion(word,root);
+    
+    }
+
+    string temp = "";
+    print(root,temp);
     
 
 }
--- codes/Trie/trie.h
+++ codes/Trie/trie.h
@@ -3,12 +3,14 @@ class Node{
 
     public:
     char data;
-    bool isterminal;
+    //bool isterminal;
+    int cnt ;
     Node * child[26];
 
     Node(char data){
         this->data = data;
-        isterminal  = false;
+        //isterminal  = false;
+        cnt = 0;
         for(int i=0;i<26;i++) this->child[i] = nullptr;
     }
 };
